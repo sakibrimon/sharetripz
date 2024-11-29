eea4e7801f0b9bf7430e0f0390afa8f65787dfc5
sakibrimon <mdsakibhassanrimon@gmail.com>
2024-11-30 05:00:49 +0600

    fixed navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -20,36 +20,38 @@ const Nav = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
+                        <li><a>Flight</a></li>
                         <li>
-                            <a>Parent</a>
+                            <a>Visa</a>
                             <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                                <li><a>Visa Application</a></li>
+                                <li><a>Visa Guide</a></li>
+                                <li><a>Transit Visa</a></li>
                             </ul>
                         </li>
-                        <li><a>Item 3</a></li>
+                        <li><a>Promotions</a></li>
                     </ul>
                 </div>
                 <a className="btn btn-ghost text-xl">ShareTripz</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
+                    <li><a>Flight</a></li>
                     <li>
                         <details>
-                            <summary>Parent</summary>
+                            <summary>Visa</summary>
                             <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                                <li><a>Visa Application</a></li>
+                                <li><a>Visa Guide</a></li>
+                                <li><a>Transit Visa</a></li>
                             </ul>
                         </details>
                     </li>
-                    <li><a>Item 3</a></li>
+                    <li><a>Promotions</a></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn">Login</a>
             </div>
         </div>
     );
